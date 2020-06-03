ef979dbf960b22afc71dbe51f37dae6c179d8133
Richard Lindhout <richardlindhout96@gmail.com>
2020-06-03 20:15:47 +0200

    Add abstraction example

--- dist/template/src/AbstractionExample.web.tsx
+++ dist/template/src/AbstractionExample.web.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
+import './AbstractionExample.web.css';
 
 export default function AbstractionExample() {
   return (
     <View style={{ backgroundColor: '#EDEDED', padding: 10 }}>
       <Text>I'm only on web devices :)</Text>
+      <div className="old-fashioned-div">
+        This is just an old fashioned div with some crazy css
+      </div>
     </View>
   );
 }
